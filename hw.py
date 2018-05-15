ba71659084bc6de68a630519d40660bfd5060e85
Rohan <rohanjosephctc@gmail.com>
2018-05-15 18:00:13 +0800

    Add shit

---
+++ hw.py
@@ -0,0 +1,33 @@
+# def div(n):
+#     i = 1
+#     while i<n:
+#         if n%i == 0:
+#             print('divisor_list',i)
+#         i = i +  1
+#
+# def filter_list(a):
+#     divisor_list = []
+#     if div(a):
+#         divisor_list.append(a)
+#     return divisor_list
+#
+# def main():
+#     a = float(input('Enter a number:'))
+#     print("divisor_list", filter_list(a))
+#
+# if __name__ == '__main__':
+#     main()
+
+def give_divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def main():
+    num = int(input("Enter a number:"))
+    print('divisors:', give_divisors(num))
+
+if __name__ == '__main__':
+    main()
